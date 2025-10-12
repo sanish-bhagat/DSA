2916466b4f62b08dc24480a6ee71a8a76eb11110
Sanish Bhagat <sanishbhagat3@gmail.com>
2025-10-12 08:25:06 +0530

    course schedule I

---
+++ graph/find_course_schedule_1/using_dfs.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// build the adjacency matrix for the jobs
+vector<vector<int>> constructAdj(int n, vector<pair<int, int>> &prerequisites)
+{
+    vector<vector<int>> adj(n);
+
+    for (auto it : prerequisites)
+    {
+        adj[it.first].push_back(it.second);
+    }
+
+    return adj;
+}
+
+bool dfs_cycle(int node, vector<vector<int>> &adj, vector<bool> &vis, vector<bool> &onPath)
+{
+    // if already vis -> return false, no cycle
+    if (vis[node])
+        return false;
+
+    // mark the curr job as visited and include it in the curr dfs path
+    vis[node] = onPath[node] = true;
+
+    // explore all the adjacent jobs
+    for (auto adjNode : adj[node])
+    {
+        // if curr adjacent job is in curr dfs path or it leads to any cycle -> return true, cycle detected
+        if (onPath[adjNode] || dfs_cycle(adjNode, adj, vis, onPath))
+            return true;
+    }
+
+    // exclude the curr job from the dfs path
+    return onPath[node] = false;
+}
+
+//! TC is O(v + e)
+//! SC is O(v)
+
+bool canFinish(int n, vector<pair<int, int>> &prerequisites)
+{
+    // build the adjacency matrix for the jobs
+    vector<vector<int>> adj = constructAdj(n, prerequisites);
+
+    // bool[], vis[] -> used for tracking visited jobs and onPath -> track jobs in curr dfs path
+    vector<bool> vis(n, false), onPath(n, false);
+
+    // iteratively traverse all the jobs
+    for (int i = 0; i < n; i++)
+    {
+        // if not visited and any cycle is detected, it means the prerequisites is not fulfilled -> return false
+        if (!vis[i] && dfs_cycle(i, adj, vis, onPath))
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int numTasks = 4;
+    vector<pair<int, int>> prerequisites;
+    prerequisites.push_back(make_pair(1, 0));
+    prerequisites.push_back(make_pair(2, 1));
+    prerequisites.push_back(make_pair(3, 2));
+    if (canFinish(numTasks, prerequisites))
+    {
+        cout << "Possible to finish all tasks";
+    }
+    else
+    {
+        cout << "Impossible to finish all tasks";
+    }
+
+    return 0;
+}
