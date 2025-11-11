f69fe6db52e94fbd7635da9182e7b51cd8db7c1f
Sanish Bhagat <sanishbhagat3@gmail.com>
2025-11-11 08:22:43 +0530

    binomial coefficient using iterative approach

---
+++ dp/nCr/iterative_approach.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//! TC is O(r)
+//! SC is O(1)
+
+int nCr(int n, int r)
+{
+    // base case
+    if (r > n)
+        return 0;
+
+    if (r == 0 || n == 1)
+        return 1;
+
+    // nCr = nC(n - r)
+    if (r > (n - r))
+        r = n - r;
+
+    long long res = 1;
+
+    // iteratively calculate the coefficient
+    for (int i = 0; i < r; i++)
+    {
+
+        // res = (res * (n - i)) / (i + 1)
+        res *= (n - i);
+        res /= (i + 1);
+    }
+
+    return (int)res;
+}
+
+int main()
+{
+    int n = 5, r = 2;
+
+    cout << "nCr = " << nCr(n, r) << endl;
+    return 0;
+}
