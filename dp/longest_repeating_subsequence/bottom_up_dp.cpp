97b5be69e9a1a8d1ca709232248c751919689b7c
Sanish Bhagat <sanishbhagat3@gmail.com>
2025-11-19 08:21:41 +0530

    memoization approach

---
+++ dp/longest_repeating_subsequence/bottom_up_dp.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int longestRepeatingSubsequence(string s) {
+}
+
+int main() {
+  
+    string s = "AABEBCDD";
+    int res = longestRepeatingSubsequence(s);
+    cout << res;
+
+    return 0;
+}
--- dp/longest_repeating_subsequence/top_down_dp.cpp
+++ dp/longest_repeating_subsequence/top_down_dp.cpp
@@ -1,28 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int helper(int i, int j, string s, vector<vector<int>> &memo) {
-    if(i == 0 || j == 0)
+int helper(int i, int j, string s, vector<vector<int>> &memo)
+{
+    // base case
+    if (i == 0 || j == 0)
         return 0;
 
-    if(memo[i][j] != -1)
+    // already computed subproblem
+    if (memo[i][j] != -1)
         return memo[i][j];
 
-    if(s[i - 1] == s[j - 1] && i != j)
+    // character matches and are at different indices
+    if (s[i - 1] == s[j - 1] && i != j)
         return memo[i][j] = 1 + helper(i - 1, j - 1, s, memo);
 
+    // character doesn't matches or are at the same index
     return memo[i][j] = max(helper(i - 1, j, s, memo), helper(i, j - 1, s, memo));
 }
 
-int longestRepeatingSubsequence(string s) {
+//! TC is O(n * n)
+//! SC is O(n * n)
+
+int longestRepeatingSubsequence(string s)
+{
     int n = s.length();
-    vector<vector<int>> memo(n + 1, vector<int> (n + 1, -1));
-    
+
+    // 2d memo[][] -> to store computed values during recursion
+    vector<vector<int>> memo(n + 1, vector<int>(n + 1, -1));
+
     return helper(n, n, s, memo);
 }
 
-int main() {
-  
+int main()
+{
+
     string s = "AABEBCDD";
     int res = longestRepeatingSubsequence(s);
     cout << res;
