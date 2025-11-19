e1a7b0f3b4d4c7e7c25039f352323fdfd70f1610
Sanish Bhagat <sanishbhagat3@gmail.com>
2025-11-19 08:25:09 +0530

    tabultaion approach

--- dp/longest_repeating_subsequence/bottom_up_dp.cpp
+++ dp/longest_repeating_subsequence/bottom_up_dp.cpp
@@ -1,12 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+//! TC is O(n * n)
+//! SC is O(n * n)
 
-int longestRepeatingSubsequence(string s) {
+int longestRepeatingSubsequence(string s)
+{
+    int n = s.length();
+
+    // 2d dp[][] table
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+    // iterate through all the characters in the string
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+
+            // character matches and are at different indices
+            if (s[i - 1] == s[j - 1] && i != j)
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+
+            // character doesn't matches or are at different indices
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[n][n];
 }
 
-int main() {
-  
+int main()
+{
+
     string s = "AABEBCDD";
     int res = longestRepeatingSubsequence(s);
     cout << res;
