b42079361337cc263a582c849100a6b01fe22f3c
Sanish Bhagat <sanishbhagat3@gmail.com>
2025-12-05 20:40:47 +0530

    memoization approach

--- dp/rod_cutting/memoization.cpp
+++ dp/rod_cutting/memoization.cpp
@@ -1,30 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int cutRodRec(int i, vector<int> &price, vector<int> &memo) {
-    if(i == 0)
+int cutRodRec(int i, vector<int> &price, vector<int> &memo)
+{
+    // rod length becomes 0 -> return profit as 0
+    if (i == 0)
         return 0;
 
-    if(memo[i] != -1)
+    // already computed subproblem
+    if (memo[i] != -1)
         return memo[i];
 
+    // recursively explore all the possible ways of cutting the rod in two pieces and
+    // compute the max profit from all of them
     int ans = 0;
-    for(int j=1; j<=i; j++)
+    for (int j = 1; j <= i; j++)
         ans = max(ans, price[j - 1] + cutRodRec(i - j, price, memo));
 
     return memo[i] = ans;
 }
 
-int cutRod(vector<int> &price) {
+//! TC is O(n * n)
+//! SC is O(n)
+
+int cutRod(vector<int> &price)
+{
     int n = price.size();
 
+    // 1d memo[] to keep track of max profit values for every rod length
     vector<int> memo(n + 1, -1);
 
     return cutRodRec(n, price, memo);
 }
 
-int main() {
-    vector<int> price =  { 1, 5, 8, 9, 10, 17, 17, 20};
+int main()
+{
+    vector<int> price = {1, 5, 8, 9, 10, 17, 17, 20};
     cout << cutRod(price);
     return 0;
 }
