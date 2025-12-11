1dc4b6542484765f1c1ddaba0be63bdd73a656b2
Sanish Bhagat <sanishbhagat3@gmail.com>
2025-12-11 08:34:18 +0530

    memoization approach

---
+++ dp/word_break_prblm/top_down_dp.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool wordBreakRec(int i, string &s, vector<string> &dictionary, vector<int> &memo)
+{
+    // traversed the string compeletely -> return true
+    if (i == s.length())
+        return true;
+
+    // already computed subproblem
+    if (memo[i] != -1)
+        return memo[i];
+
+    string prefix = "";
+
+    // iteratively try for every prefix
+    for (int j = i; j < s.length(); j++)
+    {
+        prefix += s[j];
+
+        // prefix(s[i...j)]) present in the dictionary and
+        // the rest of the string can also be broken in words -> then return true
+        if (find(dictionary.begin(), dictionary.end(), prefix) != dictionary.end() && wordBreakRec(j + 1, s, dictionary, memo))
+            return memo[i] = true;
+    }
+
+    // tried all the prefixes and none have given the soln -> return false
+    return memo[i] = false;
+}
+
+//! TC is O(n * n * m)
+//! SC is O(n)
+
+bool wordBreak(string &s, vector<string> &dictionary)
+{
+    // 1d memo[] to keep track whether the running prefix is present in the dictionary or not
+    vector<int> memo(s.length() + 1, -1);
+    return wordBreakRec(0, s, dictionary, memo);
+}
+
+int main()
+{
+    string s = "ilike";
+
+    vector<string> dictionary = {"i", "like", "gfg"};
+
+    cout << (wordBreak(s, dictionary) ? "true" : "false") << endl;
+
+    return 0;
+}
