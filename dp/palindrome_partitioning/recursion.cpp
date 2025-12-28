999de96ad6fc77dda68cd301b58c420fb4b56a3e
Sanish Bhagat <sanishbhagat3@gmail.com>
2025-12-28 15:03:08 +0530

    recursive approach

--- dp/palindrome_partitioning/recursion.cpp
+++ dp/palindrome_partitioning/recursion.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// function to return if a substring is a palindrome
 bool isPalindrome(string s, int i, int j)
 {
     while (i < j)
@@ -14,17 +15,24 @@ bool isPalindrome(string s, int i, int j)
     return true;
 }
 
+//! TC is O(n * 2^n)
+//! SC is O(n)
+
 int palPartitionRec(string s, int i, int j)
 {
+    // traversed the substring or the substring is already a palindrome -> no cuts needed
     if (i >= j || isPalindrome(s, i, j))
         return 0;
 
     int res = INT_MAX, cuts;
 
+    // try through all the possible partitions and find the minimum cuts needed
     for (int k = i; k < j; k++)
     {
+        // perform a cut and recursively for the rest of the parts
         cuts = 1 + palPartitionRec(s, i, k) + palPartitionRec(s, k + 1, j);
 
+        // pick the minimum cut
         res = min(res, cuts);
     }
 
