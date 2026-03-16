6667672275b18007993509d0a26a99f18fa31fe5
Sanish Bhagat <sanishbhagat3@gmail.com>
2026-03-16 19:05:10 +0530

    prefSum + hashmap

--- potd/gfg/k_sum_paths.cpp
+++ potd/gfg/k_sum_paths.cpp
@@ -1,71 +1,87 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node {
-  public :
+class Node
+{
+public:
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
-    Node(int val) {
+    Node(int val)
+    {
         data = val;
         left = nullptr;
         right = nullptr;
     }
 };
 
-int countRec(Node *node, int k, int sum, unordered_map<int, int> &prefSums) {
-
-    if(!node)
+int countRec(Node *node, int k, int sum, unordered_map<int, int> &prefSums)
+{
+    // base case
+    if (!node)
         return 0;
 
     int pathCount = 0;
 
-    sum += node -> data;
+    // include the curr node in the curr path sum
+    sum += node->data;
 
-    if(sum == k)
+    // path sum == target -> valid path
+    if (sum == k)
         pathCount++;
 
+    // tells the no. of paths with sum == k till curr node
     pathCount += prefSums[sum - k];
 
+    // include the curr sum in the hashmap
     prefSums[sum]++;
 
-    pathCount += countRec(node -> left, k, sum, prefSums);
-    pathCount += countRec(node -> right, k, sum, prefSums);
+    // recursively move to the left and right nodes
+    pathCount += countRec(node->left, k, sum, prefSums);
+    pathCount += countRec(node->right, k, sum, prefSums);
 
+    // exclude the curr sum from the hashmap
     prefSums[sum]--;
 
     return pathCount;
 }
 
-int countAllPaths(Node *root, int k) {
+//! TC is O(n)
+//! SC is O(n)
+
+int countAllPaths(Node *root, int k)
+{
+    // store the running prefixSum for every path
     unordered_map<int, int> prefSums;
 
+    // recursively find the total path count
     return countRec(root, k, 0, prefSums);
 }
 
-int main() {
-  
+int main()
+{
+
     // Create a sample tree:
     //        8
     //      /  \
     //     4    5
     //    / \    \
     //   3   2    2
-	//  / \   \
+    //  / \   \
     // 3  -2   1
 
-    Node* root = new Node(8);
+    Node *root = new Node(8);
     root->left = new Node(4);
     root->right = new Node(5);
     root->left->left = new Node(3);
     root->left->right = new Node(2);
     root->right->right = new Node(2);
-  	root->left->left->left = new Node(3);
-  	root->left->left->right = new Node(-2);
+    root->left->left->left = new Node(3);
+    root->left->left->right = new Node(-2);
     root->left->right->right = new Node(1);
 
-    int k = 7; 
+    int k = 7;
 
     cout << countAllPaths(root, k) << endl;
     return 0;
