0b29c3db9a5d45e4f8b9d19594074be9d3e4b1f5
Sanish Bhagat <sanishbhagat3@gmail.com>
2026-06-11 18:37:48 +0530

    keep track of closing brackets

---
+++ potd/gfg/equal_points_in_bracket.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//! TC is O(n)
+//! SC is O(1)
+
+int findIndex(string &s)
+{
+    int n = s.size();
+
+    // keep track of open and closing bracket
+    int openCnt = 0, closeCnt = 0;
+
+    // count the total no. of closing brackets
+    for (int i = 0; i < n; i++)
+    {
+
+        if (s[i] == ')')
+            closeCnt++;
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+
+        // equal point found
+        if (openCnt == closeCnt)
+            return i;
+
+        if (i < n)
+        {
+
+            // count opening brackets before next index
+            if (s[i] == '(')
+                openCnt++;
+
+            // count closing bracket from next index onwards
+            if (s[i] == ')')
+                closeCnt--;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    string s = "(())))(";
+    cout << findIndex(s);
+}
