946edc1bdc9aa581b8779bd5c3be5437df5266f0
Sanish Bhagat <sanishbhagat3@gmail.com>
2026-08-25 07:55:33 +0530

    longest consecutive subsequence

---
+++ potd/gfg/min_moves_to_sort_permutation.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minMoves(vector<int> &arr)
+{
+    int n = arr.size();
+
+    // 1d [] to keep track of longest consecutive subsequence ending at i
+    vector<int> count(n + 1, 0);
+
+    for (int x : arr)
+    {
+        // extend the subsequence
+        if (count[x - 1] != 0)
+            count[x] = count[x - 1] + 1;
+
+        // new subsequence
+        else
+            count[x] = 1;
+    }
+
+    // find the longest consecutive subsequence that are in correct related order
+    int longest = 0;
+    for (int i = 0; i <= n; i++)
+        longest = max(longest, count[i]);
+
+    // remaining elements have to be moved
+    return n - longest;
+}
+
+int main()
+{
+    vector<int> arr = {2, 1, 3};
+    cout << minMoves(arr) << endl;
+
+    arr = {4, 3, 1, 2};
+    cout << minMoves(arr) << endl;
+}
